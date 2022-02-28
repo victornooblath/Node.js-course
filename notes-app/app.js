6fb69e1c43a96cd422c4557d082f0ad697833f44
victornooblath <Paulov2001@outlook.com>
2022-02-28 15:07:05 -0300

    fim_aula12

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,5 @@
 // Its like importing another file to this actual file, the result is that node execute both of the files
-const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-console.log(getNotes())
-
-console.log(validator.isURL('google.com'))
+console.log(chalk.green.bold(getNotes()))
