2d5e5b7764fa234f570ed0ccdb2a006944ff70ad
victornooblath <Paulov2001@outlook.com>
2022-02-28 11:52:45 -0300

    fim_aula11
    added npm packages

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,7 @@
 // Its like importing another file to this actual file, the result is that node execute both of the files
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 console.log(getNotes())
+
+console.log(validator.isURL('google.com'))
