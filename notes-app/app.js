7433130517432cd4566518c81ec5c9dee8ec2571
victornooblath <Paulov2001@outlook.com>
2022-03-02 10:52:58 -0300

    fim_aula20

--- notes-app/app.js
+++ notes-app/app.js
@@ -34,8 +34,15 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing a note...')
+    builder: {
+        title: {
+            describe: 'Remove the note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
     }
 })
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function() {
     return "your notes..."
@@ -9,19 +10,29 @@ const addNote = function(title, body) {
     const duplicateNotes = notes.filter(function(note) {
         return note.title === title
     })
-
     if (duplicateNotes.length === 0) {
         notes.push({
             title: title,
             body: body
         })
         saveNotes(notes)
-        console.log('New note added!')
+        console.log(chalk.green.inverse('New note added'))
     } else {
-        console.log('Note title taken!')
+        console.log(chalk.red.inverse('Note title taken!'))
     }
+}
 
-    
+const removeNote = function(title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function(note) {
+        return note.title !== title
+    })
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No Note was removed'))
+    }
 }
 
 const saveNotes = function(notes) {
@@ -41,5 +52,6 @@ const loadNotes = function() {
 
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote
 }
